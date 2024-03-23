721775923bd93ef05f1be12fd0aa0d8c06515ca9
IlfGauhnith <lucasbburle@gmail.com>
2024-03-23 11:16:38 -0300

    filtering latin fonts

--- dataset.py
+++ dataset.py
@@ -4,6 +4,21 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from pathlib import Path
 from logger import logger
+from fontTools.ttLib import TTFont
+
+def supports_latin_alphabet(font_path):
+    try:
+        font = TTFont(font_path)
+        cmap = font.getBestCmap() or []
+        latin_chars = [chr(i) for i in range(0x0020, 0x007F)]  # Basic Latin characters
+        for char in latin_chars:
+            if ord(char) not in cmap:
+                return False
+        return True
+    except Exception as e:
+        logger.error(f"Error analyzing font '{font_path}': {e}")
+        print(f"Error analyzing font '{font_path}': {e}")
+        return False
     
 def get_fonts(path, extension='ttf'):
     """
@@ -27,9 +42,13 @@ def get_fonts(path, extension='ttf'):
     return font_paths
 
 def create_document(text, font_path, font_size=16, output_path='dataset'):
+    if not supports_latin_alphabet(font_path):
+        logger.info(f"{os.path.basename(font_path)} does not support latin alphabet.")
+        return
+            
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-    
+
     image = Image.new("RGB", (800, 200), color="white")
     draw = ImageDraw.Draw(image)
     
@@ -39,7 +58,8 @@ def create_document(text, font_path, font_size=16, output_path='dataset'):
         draw.text((5, 5), text, fill="black", font=font, language="en")
         
     except OSError as ose:
-        logger.error(f"exception:{OSError.__name__} - message:{ose} - font:{os.path.basename(font_path)}")
+        logger.error(f"Error writing document with font '{os.path.basename(font_path)}': {ose}")
+        print(f"Error writing document with font '{os.path.basename(font_path)}': {ose}")
         
     image_path = os.path.join(output_path, f"{Path(font_path).stem}_{font_size}.png")
     image.save(image_path)
@@ -48,10 +68,13 @@ def create_document(text, font_path, font_size=16, output_path='dataset'):
 
 def main():
     text_sample = """
-        If you cannot measure it, you cannot improve it.
-        When you can measure what you are speaking about, and express it in numbers,
+        "To measure is to know"
+        "If you cannot measure it, you cannot improve it."
+        "When you can measure what you are speaking about, and express it in numbers,
         you know something about it; but when you cannot measure it, when you cannot
-        express it in numbers, your knowledge is of a meagre and unsatisfactory kind.
+        express it in numbers, your knowledge is of a meagre and unsatisfactory kind."
+        
+        Lord Kelvin
     """
     fonts = get_fonts('/usr/share/fonts', 'ttf')
     
